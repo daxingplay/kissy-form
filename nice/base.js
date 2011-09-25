99930cebd6c4a8222bc5ec7d69ed8aabecfad514
minghe <minghe36@126.com>
2011-09-25 13:12:23 +0800

    del Event.trigger.

--- nice/base.js
+++ nice/base.js
@@ -4,24 +4,6 @@
  *
  **/
 KISSY.add(function(S,Event) {
-    /**
-     * 拓展KISSY.Event，trigger用于触发目标事件
-     * @param {String} target 事件目标
-     * @param {String} type 事件类型
-     */
-    Event.trigger = function(target,type){
-        var listeners = Event.__getListeners(target,type),
-            len = listeners.length,fn,that;
-        if(len == 0) return false;
-        S.each(listeners,function(listener){
-            fn = listener.fn;
-            that = listener.scope;
-            if(S.isFunction(fn)){
-                fn.call(listener.scope,{'target':target});
-            }
-        });
-    };
-
     function niceBase(){
 
     }
