e93e2011e3c3646319e5f2f1e96e285feb967591
minghe <minghe36@126.com>
2011-10-31 21:26:04 +0800

    ajaxway.

---
+++ uploader/way/ajaxWay.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview ajax方案上传
+ * @author: 剑平（明河）<minghe36@126.com>,紫英<daxingplay@gmail.com>
+ **/
+KISSY.add(function(S,Node) {
+    var EMPTY = '',$ = Node.all;
+    function AjaxWay(){
+
+    }
+    return AjaxWay;
+},{requires:['node']});
